21b82e989128e0fa58733d31f70dc3220ce6c99c
taehong <lkoiescg2031@naver.com>
2019-06-12 00:34:09 +0900

    Update How to get input.cpp

--- APSS/How to get input.cpp
+++ APSS/How to get input.cpp
@@ -28,5 +28,13 @@ int main() {
 	// 동시에 사용 불가능
 	std::ios_base::sync_with_stdio(false);
 
+	//cin을 cout으로부터 untie합니다.
+	//stream을 tie하면 다른 stream에서 입출력요청이 오기전에
+	//stream을 flush시킵니다.
+	std::cin.tie(NULL);
+
+	//cout 의 endl의 경우 버퍼를 flush 시킵니다.
+	//'\n'의 경우 버퍼를 flush 하지 않습니다.
+	std::cout << '\n';
 	return 0;
 }
