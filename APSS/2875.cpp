f113b1564e955388ef3d55d97f8b295f26b52119
taehong <lkoiescg2031@naver.com>
2019-06-24 23:03:21 +0900

    project init

--- APSS/2875.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int ans, n, m, k;
-int main() {
-	cin >> n >> m >> k;
-	while (n > 1 && m > 0 && m + n > k + 2) n -= 2, m -= 1, ans++;
-	cout << ans;
-	return 0;
-}
