ebd7032770f2d206da66e89452cfe5e19ac704f4
taehong <lkoiescg2031@naver.com>
2019-06-24 14:17:40 +0900

    Note update

--- APSS/04_Graph.cpp
+++ APSS/04_Graph.cpp
@@ -226,3 +226,139 @@ void SPFA() {
 	}
 }
 //=============================================================================
+//Ford-Pulkerson (네트워크 플로우) O(Ef)
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct MaximumFlow {
+	struct Edge {
+		int to;
+		int capacity;
+		Edge* rev;
+		Edge(int to, int capacity) : to(to), capacity(capacity) {
+		}
+	};
+	int n;
+	int source, sink;
+	vector<vector<Edge*>> graph;
+	vector<bool> check;
+	MaximumFlow(int n, int source, int sink) : n(n), source(source), sink(sink) {
+		graph.resize(n);
+		check.resize(n);
+	};
+	void add_edge(int u, int v, int cap) {
+		Edge* ori = new Edge(v, cap);
+		Edge* rev = new Edge(u, 0);
+		ori->rev = rev;
+		rev->rev = ori;
+		graph[u].push_back(ori);
+		graph[v].push_back(rev);
+	}
+	int dfs(int x, int c) {
+		if (check[x]) return 0;
+		check[x] = true;
+		if (x == sink) {
+			return c;
+		}
+		for (int i = 0; i < graph[x].size(); i++) {
+			if (graph[x][i]->capacity > 0) {
+				int nc = graph[x][i]->capacity;
+				if (c != 0 && c < nc) {
+					nc = c;
+				}
+				int f = dfs(graph[x][i]->to, nc);
+				if (f) {
+					graph[x][i]->capacity -= f;
+					graph[x][i]->rev->capacity += f;
+					return f;
+				}
+			}
+		}
+		return 0;
+	}
+	int flow() {
+		int ans = 0;
+		while (true) {
+			fill(check.begin(), check.end(), false);
+			int f = dfs(source, 0);
+			if (f == 0) break;
+			ans += f;
+		}
+		return ans;
+	}
+};
+//=============================================================================
+//Edmond-karp (네트워크 플로우) O(VE^2)
+#include <queue>
+using namespace std;
+
+struct MaximumFlow {
+	struct Edge {
+		int to;
+		int capacity;
+		Edge* rev;
+		Edge(int to, int capacity) : to(to), capacity(capacity) {
+		}
+	};
+	int n;
+	int source, sink;
+	vector<vector<Edge*>> graph;
+	MaximumFlow(int n, int source, int sink) : n(n), source(source), sink(sink) {
+		graph.resize(n);
+	};
+	void add_edge(int u, int v, int cap) {
+		Edge* ori = new Edge(v, cap);
+		Edge* rev = new Edge(u, 0);
+		ori->rev = rev;
+		rev->rev = ori;
+		graph[u].push_back(ori);
+		graph[v].push_back(rev);
+	}
+	int bfs() {
+		vector<bool> check(n, false);
+		vector<pair<int, int>> from(n, make_pair(-1, -1));
+		queue<int> q;
+		q.push(source);
+		check[source] = true;
+		while (!q.empty()) {
+			int x = q.front();
+			q.pop();
+			for (int i = 0; i < graph[x].size(); i++) {
+				if (graph[x][i]->capacity > 0 && !check[graph[x][i]->to]) {
+					q.push(graph[x][i]->to);
+					check[graph[x][i]->to] = true;
+					from[graph[x][i]->to] = make_pair(x, i);
+				}
+			}
+		}
+		if (!check[sink]) {
+			return 0;
+		}
+		int x = sink;
+		int c = graph[from[x].first][from[x].second]->capacity;
+		while (from[x].first != -1) {
+			if (c > graph[from[x].first][from[x].second]->capacity) {
+				c = graph[from[x].first][from[x].second]->capacity;
+			}
+			x = from[x].first;
+		}
+		x = sink;
+		while (from[x].first != -1) {
+			Edge* e = graph[from[x].first][from[x].second];
+			e->capacity -= c;
+			e->rev->capacity += c;
+			x = from[x].first;
+		}
+		return c;
+	}
+	int flow() {
+		int ans = 0;
+		while (true) {
+			int f = bfs();
+			if (f == 0) break;
+			ans += f;
+		}
+		return ans;
+	}
+};
